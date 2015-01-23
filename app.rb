4e40356394a60bd09eceac7155955d166e7f4116
Guillermo Esteves <g@gesteves.com>
2015-01-23 08:38:01 -0500

    Needs more Dan Chilton

--- app.rb
+++ app.rb
@@ -16,6 +16,7 @@ end
 
 get %r{/(\d+)/(\d+)} do
   photos = %w{
+    https://farm8.staticflickr.com/7549/16151236488_08bd4c62b1_o.jpg
     https://farm8.staticflickr.com/7554/16152643219_51b34b9596_o.jpg
     https://farm8.staticflickr.com/7536/16312881956_8aebcc8bd7_o.jpg
     https://farm9.staticflickr.com/8644/16337948952_aa73bc9ae0_o.jpg
